91499810ed8bb3c494fc008ac4a75e7930b94138
Matthew Sheldon <msheldo@iu.edu>
2025-04-22 20:30:43 -0400

    Implemented collision correctly

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Controller2D))]
-public class Player : MonoBehaviour
-{
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
 
 	public float maxJumpHeight = 4;
 	public float minJumpHeight = 1;
 	public float timeToJumpApex = .4f;
 	float accelerationTimeAirborne = .2f;
 	float accelerationTimeGrounded = .1f;
-	float moveSpeed = 6;
+	public float moveSpeed = 6;
 
 	public Vector2 wallJumpClimb;
 	public Vector2 wallJumpOff;
@@ -32,114 +31,89 @@ public class Player : MonoBehaviour
 	bool wallSliding;
 	int wallDirX;
 
-	void Start()
-	{
-		controller = GetComponent<Controller2D>();
+	void Start() {
+		controller = GetComponent<Controller2D> ();
 
-		gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
 		maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-		minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+		minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
 	}
 
-	void Update()
-	{
-		CalculateVelocity();
-		HandleWallSliding();
+	void Update() {
+		CalculateVelocity ();
+		HandleWallSliding ();
 
-		controller.Move(velocity * Time.deltaTime, directionalInput);
+		controller.Move (velocity * Time.deltaTime, directionalInput);
 
-		if (controller.collisions.above || controller.collisions.below)
-		{
-			if (controller.collisions.slidingDownMaxSlope)
-			{
+		if (controller.collisions.above || controller.collisions.below) {
+			if (controller.collisions.slidingDownMaxSlope) {
 				velocity.y += controller.collisions.slopeNormal.y * -gravity * Time.deltaTime;
-			}
-			else
-			{
+			} else {
 				velocity.y = 0;
 			}
 		}
 	}
 
-	public void SetDirectionalInput(Vector2 input)
-	{
+	public void SetDirectionalInput (Vector2 input) {
 		directionalInput = input;
 	}
 
-	public void OnJumpInputDown()
-	{
-		if (wallSliding)
-		{
-			if (wallDirX == directionalInput.x)
-			{
+	public void OnJumpInputDown() {
+		if (wallSliding) {
+			if (wallDirX == directionalInput.x) {
 				velocity.x = -wallDirX * wallJumpClimb.x;
 				velocity.y = wallJumpClimb.y;
 			}
-			else if (directionalInput.x == 0)
-			{
+			else if (directionalInput.x == 0) {
 				velocity.x = -wallDirX * wallJumpOff.x;
 				velocity.y = wallJumpOff.y;
 			}
-			else
-			{
+			else {
 				velocity.x = -wallDirX * wallLeap.x;
 				velocity.y = wallLeap.y;
 			}
 		}
-		if (controller.collisions.below)
-		{
-			if (controller.collisions.slidingDownMaxSlope)
-			{
-				if (directionalInput.x != -Mathf.Sign(controller.collisions.slopeNormal.x))
-				{ // not jumping against max slope
+		if (controller.collisions.below) {
+			if (controller.collisions.slidingDownMaxSlope) {
+				if (directionalInput.x != -Mathf.Sign (controller.collisions.slopeNormal.x)) { // not jumping against max slope
 					velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
 					velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
 				}
-			}
-			else
-			{
+			} else {
 				velocity.y = maxJumpVelocity;
 			}
 		}
 	}
 
-	public void OnJumpInputUp()
-	{
-		if (velocity.y > minJumpVelocity)
-		{
+	public void OnJumpInputUp() {
+		if (velocity.y > minJumpVelocity) {
 			velocity.y = minJumpVelocity;
 		}
 	}
+		
 
-	void HandleWallSliding()
-	{
+	void HandleWallSliding() {
 		wallDirX = (controller.collisions.left) ? -1 : 1;
 		wallSliding = false;
-		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
-		{
+		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0) {
 			wallSliding = true;
 
-			if (velocity.y < -wallSlideSpeedMax)
-			{
+			if (velocity.y < -wallSlideSpeedMax) {
 				velocity.y = -wallSlideSpeedMax;
 			}
 
-			if (timeToWallUnstick > 0)
-			{
+			if (timeToWallUnstick > 0) {
 				velocityXSmoothing = 0;
 				velocity.x = 0;
 
-				if (directionalInput.x != wallDirX && directionalInput.x != 0)
-				{
+				if (directionalInput.x != wallDirX && directionalInput.x != 0) {
 					timeToWallUnstick -= Time.deltaTime;
 				}
-				else
-				{
+				else {
 					timeToWallUnstick = wallStickTime;
 				}
 			}
-			else
-			{
+			else {
 				timeToWallUnstick = wallStickTime;
 			}
 
@@ -147,10 +121,9 @@ public class Player : MonoBehaviour
 
 	}
 
-	void CalculateVelocity()
-	{
+	void CalculateVelocity() {
 		float targetVelocityX = directionalInput.x * moveSpeed;
-		velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
 		velocity.y += gravity * Time.deltaTime;
 	}
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,29 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Player))]
-public class PlayerInput : MonoBehaviour
-{
+[RequireComponent (typeof (Player))]
+public class PlayerInput : MonoBehaviour {
 
 	Player player;
 
-	void Start()
-	{
-		player = GetComponent<Player>();
+	void Start () {
+		player = GetComponent<Player> ();
 	}
 
-	void Update()
-	{
-		Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		player.SetDirectionalInput(directionalInput);
+	void Update () {
+		Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		player.SetDirectionalInput (directionalInput);
 
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			player.OnJumpInputDown();
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			player.OnJumpInputDown ();
 		}
-		if (Input.GetKeyUp(KeyCode.Space))
-		{
-			player.OnJumpInputUp();
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			player.OnJumpInputUp ();
 		}
 	}
 }
