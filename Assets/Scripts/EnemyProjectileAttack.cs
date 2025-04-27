54d1d846f775c43a160269c32ac470cdcf618550
Squidskull-2 <msheldo@iu.edu>
2025-04-27 04:12:50 -0400

    Made projectile enemy stationary, adjusted box colliders
    ChargingEnemy tag added for future bug fixing/feature implementation - not currently needed.

--- Assets/Scripts/EnemyProjectileAttack.cs
+++ Assets/Scripts/EnemyProjectileAttack.cs
@@ -8,18 +8,32 @@ public class EnemyProjectileAttack : MonoBehaviour
     public float attackTimer = 3f;
     EnemyMovement movement;
     public GameObject bullet;
+    float timer = 0;
+    bool playerInRange;
 
     // Start is called before the first frame update
     void Start()
     {
         movement = GetComponent<EnemyMovement>();
-        InvokeRepeating("ShootProjectile", 1f, attackTimer);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Vector3.Distance(GameObject.FindWithTag("Player").transform.position, transform.position) <= 5f)
+        {
+            playerInRange = true;
+        }
+        else
+        {
+            playerInRange = false;
+        }
+        timer += Time.deltaTime;
+        if (timer >= attackTimer && playerInRange)
+        {
+            timer = 0f;
+            ShootProjectile();
+        }
     }
 
     void ShootProjectile()
